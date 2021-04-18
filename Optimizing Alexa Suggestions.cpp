51b2aedc0b22eb12713688369cae35c7ec02285f
Afzal Ansari <afzal442@gmail.com>
2021-04-18 23:57:27 +0530

    Create Optimizing Alexa Suggestions.cpp

---
+++ Optimizing Alexa Suggestions.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+
+
+/*
+ * Complete the 'findRestaurants' function below.
+ *
+ * The function is expected to return a 2D_INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. 2D_INTEGER_ARRAY allLocations
+ *  2. INTEGER numRestaurants
+ */
+
+vector<vector<int>> findRestaurants(vector<vector<int>> allLocations, int numRestaurants) {
+
+}
+
+int main()
