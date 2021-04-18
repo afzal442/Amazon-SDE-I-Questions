26bbe2d1757cdd24bd6e0cbd358dc0dd3f854b8f
Afzal Ansari <afzal442@gmail.com>
2021-04-18 23:58:34 +0530

    Create Order Delivery Route.cpp

---
+++ Order Delivery Route.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+
+
+/*
+ * Complete the 'minimumDistance' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY area as parameter.
+ */
+
+int minimumDistance(vector<vector<int>> area) {
+
+}
+
+int main()
